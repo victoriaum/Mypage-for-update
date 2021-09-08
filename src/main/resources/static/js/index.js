8d9561602cf642799f5c975ec76c16758280d598
victoriaum <y.ilpyo95@gmail.com>
2021-09-09 00:01:39 +0900

    포트폴리오 및 css 수정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,1 +1,14 @@
-$(function(){}
+/* 초기설정 */
+$(function(){
+  $("#korean").addClass("selectedLang");
+})
+
+/* 언어 선택에 따른 색상변경 */
+$("#korean").click(function(){
+  $(this).addClass("selectedLang");
+  $("#english").removeClass("selectedLang");
+})
+$("#english").click(function(){
+  $(this).addClass("selectedLang");
+  $("#korean").removeClass("selectedLang");
+})
