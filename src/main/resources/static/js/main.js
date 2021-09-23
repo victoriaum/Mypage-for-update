bb704fa62fbeeda23cc75e455ca9f29be880919b
victoriaum <y.ilpyo95@gmail.com>
2021-09-23 22:39:18 +0900

    모달창 변경 및 스크롤이미지 추가

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -248,4 +248,33 @@ jQuery(document).ready(function($) {
 	  return false;
 	});
 
+	let owl = $('.owl-carousel');
+	owl.owlCarousel({
+		loop:true,
+		nav:true,
+		margin:10,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			960:{
+				items:1
+			},
+			1200:{
+				items:1
+			}
+		}
+	});
+	owl.on('mousewheel', '.owl-stage', function (e) {
+		if (e.deltaY>0) {
+			owl.trigger('prev.owl');
+		} else {
+			owl.trigger('next.owl');
+		}
+		e.preventDefault();
+	});
+
 });
